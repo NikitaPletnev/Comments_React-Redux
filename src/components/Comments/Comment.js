166f3a23158491d8b30fcc86d65f2fac80aa421d
Плетнев Никита Сергеевич <45274552+NikitaPletnev@users.noreply.github.com>
2019-10-07 18:54:05 +0300

    Теперь теги не отображаются

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -8,9 +8,9 @@ export default ({ comment: { author, text, id }, onDelete}) => {
     return (
         <div class="comments-item">
         <div className="comments-item__captions">
-            <div className="author">{ author }</div>
+            <div className="author">{ author.replace(/(\<(\/?[^>]+)>)/g,"") }</div>
         </div>    
-            <p className="comments-item__text">{ text }</p>
+            <p className="comments-item__text">{ text.replace(/(\<(\/?[^>]+)>)/g,"") }</p>
             <button className="btn btn-danger" type="button" onClick={() => onDelete(id)}>
                 Удалить
             </button>
